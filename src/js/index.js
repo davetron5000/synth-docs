75813a78c697e9a6bfb5d0cd8d8b9507ad143c15
Dave Copeland <davec@naildrivin5.com>
2024-07-29 20:27:15 -0400

    add utils

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,42 @@
-class HomescreenNotification extends HTMLElement {
+class BaseCustomElement extends HTMLElement {
+
+  #domLoaded
+  #onDOMLoaded
+
+  constructor() {
+    super()
+    this.#domLoaded = false
+    this.#onDOMLoaded = () => {
+      this.#domLoaded = true
+      this.#_render()
+    }
+  }
+
+  connectedCallback() {
+    document.addEventListener("DOMContentLoaded", this.#onDOMLoaded)
+    if (this.onConnected) {
+      this.onConnected()
+    }
+    this.#_render()
+  }
+
+  #_render() {
+    if (!this.#domLoaded) {
+      return
+    }
+    if (this.render)  {
+      this.render()
+    }
+  }
+}
+
+class HomescreenNotification extends BaseCustomElement {
 
   #dismiss
 
   constructor() {
     super()
-    this.style.display = "none"
+        window.localStorage.removeItem("seenHomeScreenMessage")
     this.#dismiss = (event) => {
       event.preventDefault()
       this.style.display = "none"
@@ -14,11 +46,11 @@ class HomescreenNotification extends HTMLElement {
     }
   }
 
-  connectedCallback() {
-    this.#render()
+  onConnected() {
+    this.style.display = "none"
   }
 
-  #render() {
+  render() {
     const button = this.querySelector("button")
 
     const isAddedToHomeScreen = window.matchMedia("(display-mode: standalone)").matches
@@ -35,12 +67,10 @@ class HomescreenNotification extends HTMLElement {
     button.addEventListener("click",this.#dismiss)
   }
 }
-class PWADetection extends HTMLElement {
-  connectedCallback() {
-    this.#render()
-  }
 
-  #render() {
+class PWADetection extends BaseCustomElement {
+
+  render() {
     const isAddedToHomeScreen = window.matchMedia("(display-mode: standalone)").matches
     this.querySelectorAll("[data-if-not-pwa]").forEach( (element) => {
       if (isAddedToHomeScreen) {
@@ -62,7 +92,7 @@ class PWADetection extends HTMLElement {
   }
 }
 
-class ReloadPage extends HTMLElement {
+class ReloadPage extends BaseCustomElement {
 
   #reloadPage
 
@@ -73,13 +103,85 @@ class ReloadPage extends HTMLElement {
       window.location.reload()
     }
   }
-  connectedCallback() {
+  render() {
     this.querySelector("button").addEventListener("click", this.#reloadPage)
   }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  customElements.define("synth-homescreen-notification",HomescreenNotification)
-  customElements.define("synth-pwa-detection",PWADetection)
-  customElements.define("synth-reload",ReloadPage)
-})
+class BPMCalc extends BaseCustomElement {
+  #updateCalculations
+  #submitForm
+
+  constructor() {
+    super()
+    this.#submitForm = (event) => {
+      event.preventDefault()
+      const input = event.currentTarget.querySelector("input[name=bpm]")
+      if (input) {
+        this.#calculate(input.value)
+      }
+    }
+    this.#updateCalculations = (event) => {
+      if (event.currentTarget.value) {
+        this.#calculate(event.currentTarget.value)
+      }
+      else {
+        this.#clearAll()
+      }
+    }
+  }
+
+  render() {
+    const input = this.querySelector("input[name=bpm]")
+    if (input) {
+      if (input.value) {
+        this.#calculate(input.value)
+      }
+      else {
+        this.#clearAll()
+      }
+      input.addEventListener("input", this.#updateCalculations)
+      input.focus()
+    }
+    else {
+      this.#clearAll()
+    }
+    const form = this.querySelector("form")
+    if (form) {
+      form.addEventListener("submit", this.#submitForm)
+      form.querySelectorAll("input[type=submit]").forEach( (element) => {
+        element.style.display = "none"
+      })
+    }
+  }
+
+  #calculate(value) {
+    const bpm = Number(value)
+    if (isNaN(bpm)) {
+      this.#clearAll()
+      return
+    }
+    this.querySelectorAll("[data-note]").forEach( (element) => {
+      const divisor = Number(element.dataset.note)
+      if (!isNaN(divisor)) {
+        const ms = (60000 / bpm) / divisor
+        element.textContent = ms.toLocaleString(undefined,{ minimumFractionDigits: 0, maximumFractionDigits: 0, useGrouping: false })
+      }
+      else {
+        console.warn("%o's data-note was not a number: %s",element,element.dataset.note)
+      }
+    })
+  }
+
+  #clearAll() {
+    this.querySelectorAll("[data-note]").forEach( (element) => {
+      element.textContent = ""
+    })
+  }
+}
+
+
+customElements.define("synth-homescreen-notification",HomescreenNotification)
+customElements.define("synth-pwa-detection",PWADetection)
+customElements.define("synth-reload",ReloadPage)
+customElements.define("synth-bpm-calc",BPMCalc)
